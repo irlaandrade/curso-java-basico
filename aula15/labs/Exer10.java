7df122e8bf4956ae5e17a744598168f0724951c4
irlaandrade <paula.irla@gmail.com>
2021-02-21 19:40:23 -0300

    Controle de decisão e exercícios

---
+++ aula15/labs/Exer10.java
@@ -0,0 +1,25 @@
+package com.loiane.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer10 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o turno que você estuda (M-matutino ou V-Vespertino ou N- Noturno):");
+		String turno = scan.next();
+		
+		switch (turno) {
+		case "m":
+		case "M": System.out.println("Bom dia!"); break;
+		case "v":
+		case "V": System.out.println("Boa tarde!"); break;
+		case "n":
+		case "N": System.out.println("Boa noite!"); break;
+		default: System.out.println("Valor inválido.");
+		}
+	}
+
+}
