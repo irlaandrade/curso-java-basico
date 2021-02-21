99733cbd15f27680617083bc09417d57d527fce1
irlaandrade <paula.irla@gmail.com>
2021-02-21 19:43:21 -0300

    Arrays, matrizes, foreach e exercícios

---
+++ aula19/labs/Exer01.java
@@ -0,0 +1,37 @@
+package com.loiane.cursojava.aula19.labs;
+
+import java.util.Scanner;
+
+/*Criar um vetor A com 5 elementos inteiros. Construir um vetor B de 
+mesmo tipo e tamanho e com os "mesmos" elementos do vetor A, ou 
+seja, B[i] = A[i].*/
+
+public class Exer01 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] vetorA = new int[5];
+		int[] vetorB = new int[vetorA.length];
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com o valor da posição: " + i);
+			vetorA[i] = scan.nextInt();
+			
+			vetorB[i] = vetorA[i];
+		}
+		
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.print("Vetor B = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorB[i] + " ");
+		}
+		System.out.println();
+	}
+}
---
+++ aula19/labs/Exer05.java
@@ -0,0 +1,44 @@
+package com.loiane.cursojava.aula19.labs;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*Criar um vetor A com 10 elementos inteiros. Construir um vetor B de 
+mesmo tipo e tamanho, sendo que cada elemento do vetor B deverá 
+ser o respectivo elemento de A multiplicado por sua posição (ou 
+índice), ou seja: 
+B[i] = A[i] * i.*/
+
+public class Exer05 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com o valor da posição: " + i);
+			vetorA[i] = scan.nextInt();
+
+			vetorB[i] = vetorA[i] * i;
+		}
+
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+
+		DecimalFormat df = new DecimalFormat("###,###.##");
+		
+		System.out.print("Vetor B = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(df.format(vetorB[i]) + " ");
+		}
+		System.out.println();
+
+	}
+
+}
