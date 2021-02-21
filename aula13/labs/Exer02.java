a9663bc66b46b1e0c1e72d097d892395602b659e
irlaandrade <paula.irla@gmail.com>
2021-02-21 19:38:52 -0300

    Operadores e exercícios

---
+++ aula13/labs/Exer02.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Scanner;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com um número inteiro:");
+		int numero = scan.nextInt();
+		System.out.println("O número informado foi: " + numero);
+		
+	}
+
+}
---
+++ aula13/labs/Exercicio1.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula13.labs;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alo Mundo");
+
+	}
+
+}
