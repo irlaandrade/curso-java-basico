b77407a337b53dc9d7782f487d7df002e4844865
irlaandrade <paula.irla@gmail.com>
2021-03-09 08:18:21 -0300

    Classes abstratas

---
+++ aula41/Aluno.java
@@ -0,0 +1,62 @@
+package com.loiane.cursojava.aula41;
+
+public class Aluno extends Pessoa {
+
+	private String curso;
+	private double[] notas;
+	
+	public void verificarAcesso() {
+		super.nomeVisibilidade = "Maria";
+		
+		this.nomeVisibilidade = "Maria";
+	}
+	
+	public Aluno() {
+		super();
+	}
+
+	public Aluno(String nome, String endereco, String telefone, String curso) {
+		super(nome, endereco, telefone);
+		this.curso = curso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+	
+	public double calcularMedia() {
+		return 0;
+	}
+	
+	public boolean verificarAprovado() {
+		return true;
+	}
+	
+	public void metodoQualquer() {
+		super.setCpf("12345678");
+	}
+	
+	public String obterEtiquetaEndereco() {
+		
+		String s = "Endereço do Aluno: ";
+		s += super.getEndereco();
+		
+		return s;
+	}
+
+	@Override
+	public void imprimirEtiquetaEndereco() {
+
+		System.out.println(this.obterEtiquetaEndereco());
+		
+	}
+}
