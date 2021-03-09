d2a7a931e9b0efc1ce34e862a6cfc48e131a7499
irlaandrade <paula.irla@gmail.com>
2021-03-09 08:13:44 -0300

    Recursividade

---
+++ aula35/labs/TesteFibonacci.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula35.labs;
+
+public class TesteFibonacci {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.print(Fibonacci.fibonacci(i) + " ");
+		}
+	}
+
+}
